e6d2740d78e4f604778f25ff839a8402c6c7f085
Alyan Shahid <alyanshahidsdk@gmail.com>
2026-02-15 16:20:33 +0500

    feat: learned how to add TypeSaftey in funcations

--- app.ts
+++ app.ts
@@ -2,4 +2,23 @@ let greeting: string = "Hello"
 
 console.log(greeting);
 
+
+
+//funcation
+
+function name(name: string): string {
+
+    return `hello ${name}`;
+
+}
+
+const add = (a: number, b: number): number => {
+
+    return a + b;
+}
+
+console.log(add(2, 3))
+
+name("alyan shahid");
+
 export { }
