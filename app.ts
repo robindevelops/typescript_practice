6efacd8d4377b44ba7bc7b612f905fc2ba99fc72
Alyan Shahid <alyanshahidsdk@gmail.com>
2026-02-15 17:36:09 +0500

    feat: add `createUser` function to check howw TypeSaftey work on objects

--- app.ts
+++ app.ts
@@ -21,4 +21,17 @@ console.log(add(2, 3))
 
 name("alyan shahid");
 
+
+//Objects
+
+// ✅ Method 1: Inline Object Type (for function parameters)
+function createUser({ name, email, isPaid }: { name: string; email: string; isPaid: boolean }) {
+    console.log(`User: ${name}, Email: ${email}, Paid: ${isPaid}`);
+}
+
+createUser({ name: "alyan", email: "alyan@example.com", isPaid: true })
+
+
+
+//
 export { }
